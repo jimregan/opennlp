9641419ddea486582f3144b60a8e4fd6dadb1b38
Jim O'Regan <joregan@gmail.com>
2017-04-30 16:41:43 +0100

    debug

--- opennlp-tools/src/main/java/opennlp/tools/formats/irishsentencebank/IrishSentenceBankDocument.java
+++ opennlp-tools/src/main/java/opennlp/tools/formats/irishsentencebank/IrishSentenceBankDocument.java
@@ -225,9 +225,7 @@ public class IrishSentenceBankDocument {
 
             String tkn = sentnl.item(j).getAttributes().getNamedItem("lemma").getNodeValue();
             flx.get(slot).add(tkn);
-            if (flx == null) {
-              throw new IOException("WTF? " + flx.size() + flx.keySet().toString() + slot + tkn);
-            }
+            throw new IOException("WTF? " + flx.size() + flx.keySet().toString() + slot + tkn);
           } else if (name.equals("#text") || name.equals("#comment")) {
             continue;
           } else {
